7b6a5e701588d60bc3df6dbfc9c05d4662592335
mohamkhamis209-hub <mohamkhamis209@gmail.com>
2026-06-16 13:37:06 +0300

    Add files via upload

---
+++ js/device.js
@@ -0,0 +1,16 @@
+export function getDeviceId() {
+    let deviceId = localStorage.getItem('deviceId');
+    if (!deviceId) {
+        const data = [
+            navigator.userAgent,
+            screen.width,
+            screen.height,
+            navigator.language,
+            new Date().getTimezoneOffset()
+        ].join('||');
+        // تشفير بسيط base64
+        deviceId = btoa(data).slice(0, 20);
+        localStorage.setItem('deviceId', deviceId);
+    }
+    return deviceId;
+}
